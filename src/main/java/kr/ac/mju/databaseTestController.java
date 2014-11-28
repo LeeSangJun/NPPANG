00440866a1631f218c1cb03b8b692cd55503dae8
LeeSangjun <shaamy@naver.com>
2014-11-28 15:32:23 +0900

    Mybatis 3.0.6 setting

---
+++ src/main/java/kr/ac/mju/databaseTestController.java
@@ -0,0 +1,42 @@
+package kr.ac.mju;
+
+import kr.ac.mju.dao.testDAO;
+import kr.ac.mju.model.test;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class databaseTestController {
+	private static final Logger logger = LoggerFactory.getLogger(LoginController.class);
+
+	/**
+	 * Simply selects the home view to render by returning its name.
+	 */
+
+//	@Autowired
+//	private testService testservice;
+
+	@Autowired
+	private testDAO dao;
+
+	@RequestMapping(value = "/testMybatis", method = RequestMethod.GET)
+	public ModelAndView testDatabase() {
+		ModelAndView DBview = new ModelAndView();
+
+//		testDAO dao = new testDAO();
+		test model;
+		model = dao.getTestModel();
+//		model = testservice.getTest();
+		if(model != null){
+			DBview.addObject("test", model.getName());
+			DBview.setViewName("DBview");
+		}
+		return DBview;
+	}
+}
---
+++ src/main/java/kr/ac/mju/model/test.java
@@ -0,0 +1,12 @@
+package kr.ac.mju.model;
+
+public class test {
+	String name;
+	String title;
+
+	public void setName(String name){this.name = name;}
+	public String getName(){return name;}
+
+	public void setTitle(String title){this.title = title;}
+	public String getTitle(){return title;}
+}
