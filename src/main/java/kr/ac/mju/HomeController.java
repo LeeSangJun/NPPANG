e5d971e5f6fa42e53f92b8b2390695671a390564
Shinseungsou <wwww432@lycos.co.kr>
2014-12-02 21:14:01 +0900

    db 추가
    user_info 검색 테스트 코드 삽�

--- src/main/java/kr/ac/mju/HomeController.java
+++ src/main/java/kr/ac/mju/HomeController.java
@@ -17,39 +17,31 @@ import org.springframework.web.bind.annotation.RequestMethod;
  */
 @Controller
 public class HomeController {
-	
+
 	private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
-	
+
 	/**
 	 * Simply selects the home view to render by returning its name.
 	 */
 	@RequestMapping(value = "/", method = RequestMethod.GET)
 	public String home(Locale locale, Model model) {
 		logger.info("Welcome home! The client locale is {}.", locale);
-		
-		Date date = new Date();
-		DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
-		
-		String formattedDate = dateFormat.format(date);
-		
-		model.addAttribute("serverTime", formattedDate );
-		
-	
+
 		return "index";
 	}
-	
+
 	@RequestMapping(value = "/{viewname}/", method = RequestMethod.GET)
 	public String layout_router(@PathVariable String viewname) {
 		System.out.println("url" + viewname);
 		return viewname;
 	}
-	
-	
+
+
 	@RequestMapping(value = "/layout", method = RequestMethod.GET)
 	public String layout() {
 		System.out.println("update test");
 		return "layout_v_3";
 	}
-	
-	
+
+
 }
