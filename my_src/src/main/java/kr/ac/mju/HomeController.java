5c5ff6e819c4467a849232c8dfe9cf58c404e1c6
Shinseungsou <wwww432@lycos.co.kr>
2014-12-02 21:40:02 +0900

    it is sss file
    it is sss file

---
+++ my_src/src/main/java/kr/ac/mju/HomeController.java
@@ -0,0 +1,47 @@
+package kr.ac.mju;
+
+import java.text.DateFormat;
+import java.util.Date;
+import java.util.Locale;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+/**
+ * Handles requests for the application home page.
+ */
+@Controller
+public class HomeController {
+
+	private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
+
+	/**
+	 * Simply selects the home view to render by returning its name.
+	 */
+	@RequestMapping(value = "/", method = RequestMethod.GET)
+	public String home(Locale locale, Model model) {
+		logger.info("Welcome home! The client locale is {}.", locale);
+
+		return "index";
+	}
+
+	@RequestMapping(value = "/{viewname}/", method = RequestMethod.GET)
+	public String layout_router(@PathVariable String viewname) {
+		System.out.println("url" + viewname);
+		return viewname;
+	}
+
+
+	@RequestMapping(value = "/layout", method = RequestMethod.GET)
+	public String layout() {
+		System.out.println("update test");
+		return "layout_v_3";
+	}
+
+
+}
---
+++ my_src/src/main/java/kr/ac/mju/LoginController.java
@@ -0,0 +1,72 @@
+package kr.ac.mju;
+
+import java.util.Locale;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+
+import kr.ac.mju.dao.userDAO;
+import kr.ac.mju.model.User_Info;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+
+/**
+ * Handles requests for the application home page.
+ */
+@Controller
+public class LoginController {
+
+	private static final Logger logger = LoggerFactory.getLogger(LoginController.class);
+
+	/**
+	 * Simply selects the home view to render by returning its name.
+	 */
+	@RequestMapping(value = "/login", method = RequestMethod.POST)
+	public ModelAndView home(Model model, HttpSession session, HttpServletRequest reqest, @RequestParam("userId") String userId) {
+		ModelAndView modelAndView = new ModelAndView();
+		if(session.getAttribute("userID") == null){
+			if(userId.equals("admin")){
+				session.setAttribute("userId", userId);
+				modelAndView.setViewName("index");
+			}else{
+				modelAndView.addObject("error", "Id를 찾을 수 없습니다.");
+				modelAndView.setViewName("index");
+				return modelAndView;
+			}
+		}else{
+			modelAndView.addObject("error", "이미 로그인되어 있습니다.");
+			modelAndView.setViewName("index");
+			return modelAndView;
+		}
+
+		return modelAndView;
+	}
+	@RequestMapping(value = "/logout", method = RequestMethod.POST)
+	public ModelAndView logout(HttpSession session, HttpServletRequest reqest) {
+		ModelAndView modelAndView = new ModelAndView();
+		session.invalidate();
+		modelAndView.setViewName("index");
+		return modelAndView;
+	}
+
+	@Autowired
+	userDAO dao;
+
+	@RequestMapping(value = "/testuser", method = RequestMethod.GET)
+	public ModelAndView getUser(HttpSession session, HttpServletRequest reqest){
+		ModelAndView modelAndView = new ModelAndView();
+		User_Info user = dao.getUserId(1);
+		modelAndView.addObject("name", user.getName());
+		modelAndView.setViewName("usertest");
+
+		return modelAndView;
+	}
+}
