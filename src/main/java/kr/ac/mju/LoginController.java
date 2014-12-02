74f3f5056b6b29ef994879846223ec35da9ca307
Shinseungsou <wwww432@lycos.co.kr>
2014-12-02 21:45:18 +0900

    error fix
    error fix

--- src/main/java/kr/ac/mju/LoginController.java
+++ src/main/java/kr/ac/mju/LoginController.java
@@ -1,12 +1,14 @@
 package kr.ac.mju;
 
-import java.util.Locale;
-
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 
+import kr.ac.mju.dao.userDAO;
+import kr.ac.mju.model.User_Info;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
